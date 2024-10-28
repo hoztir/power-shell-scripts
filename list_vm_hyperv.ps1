3ca66624402d1ad1611f328d2b45f8e8e06cd935
Luiz Augusto Viana Sales <luiz.sales@ocrim.sa>
2024-10-28 11:06:53 -0300

    Adicionado as colunas de CPU e RAM alocada no relatório list_vm_hyperv

--- list_vm_hyperv.ps1
+++ list_vm_hyperv.ps1
@@ -6,10 +6,12 @@ Get-VM | ForEach-Object {
         [PSCustomObject]@{
             VMName            = $vm.Name
             State             = $vm.State
+            CPU               = $vm.ProcessorCount
+            RAM               = [math]::round($vm.MemoryAssigned  / 1GB, 2)
             DiskPath          = $_.Path
             AllocatedSizeGB   = [math]::round($vhd.FileSize / 1GB, 2)
             UsedSizeGB        = [math]::round(($vhd.FileSize - $vhd.SavedSpace) / 1GB, 2)
             IPAddress         = $ipAddress -join ', '  # Junte múltiplos IPs, se houver
         }
     }
-} | Select-Object VMName, State, DiskPath, AllocatedSizeGB, UsedSizeGB, IPAddress | Export-Csv -Path C:\VMs_Info.csv -NoTypeInformation -Delimiter ';'
+} | Select-Object VMName, State, CPU, AllocatedRAM_GB, DiskPath, AllocatedSizeGB, UsedSizeGB, IPAddress | Export-Csv -Path C:\VMs_Info.csv -NoTypeInformation -Delimiter ';'
